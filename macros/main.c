1170fced74a53378ea7b57047f96ecd0e68445c2
unknown <atommyx@gmail.com>
2022-04-03 23:10:41 -0300

    note

--- macros/main.c
+++ macros/main.c
@@ -3,6 +3,8 @@
 #define PARTE_ENTERA(X) (int)(X)
 #define PARTE_DECIMAL(X) (X)-PARTE_ENTERA(X)
 #define REDONDEO(X) PARTE_DECIMAL>0.5?PARTE_ENTERA(X+1):PARTE_ENTERA(X)
+//#define
+//abs mayor menor es_par es_impar es_letra es_nro es_blanco es_mayus es_minus a_mayus a_minus es_vocal
 
 
 
