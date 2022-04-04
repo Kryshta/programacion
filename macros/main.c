a3554dc48464ddf209e6700f69047e3e3feb9e60
unknown <atommyx@gmail.com>
2022-04-03 22:44:44 -0300

    first commit

---
+++ macros/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PARTE_ENTERA(X) (int)(X)
+#define PARTE_DECIMAL(X) (X)-PARTE_ENTERA(X)
+#define REDONDEO(X) PARTE_DECIMAL>0.5?PARTE_ENTERA(X):PARTE_ENTERA(X+1)
+
+
+
+int main()
+{
+    printf("%d \n", PARTE_ENTERA(5.05));
+    printf("%f \n", PARTE_DECIMAL(5.05));
+
+    return 0;
+
+}
+
